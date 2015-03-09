8da814adcfb149467c44cc8bb8229d2cf4defcd3
Craig Weber <webercraigm@johndeere.com>
2015-03-09 16:45:54 -0500

    Added 'episodes' command and better command-line interface

---
+++ rss/helptmpl.go
@@ -0,0 +1,19 @@
+package main
+
+const apphelptmpl = `{{if .Name}}NAME:   {{ "\n    " }}{{.Name   }}{{ "\n" }}
+{{end          }}{{if .Usage   }}USAGE:  {{ "\n    " }}{{.Usage  }}{{ "\n" }}
+{{end          }}{{if .Version }}VERSION:{{ "\n    " }}{{.Version}}{{ "\n" }}
+{{end          }}{{if .Author  }}AUTHOR: {{ "\n    " }}{{.Author }}{{ "\n" }}
+{{end          }}{{if .Email   }}EMAIL:  {{ "\n    " }}{{.Email  }}{{ "\n" }}
+{{end          }}{{if .Commands}}COMMANDS:
+    {{range .Commands}}{{.Name}}{{with .ShortName}}, {{.}}{{end}}{{ "\t"}}{{.Description}}
+    {{end}}{{end}}
+`
+
+const cmdhelptmpl = `{{if .Name}}NAME:        {{ "\n    "}}{{.Name       }}{{ "\n" }}
+{{end}}{{if              .Usage}}USAGE:       {{ "\n    "}}{{.Usage      }}{{ "\n" }}
+{{end}}{{if        .Description}}DESCRIPTION: {{ "\n    "}}{{.Description}}{{ "\n" }}
+{{end}}{{if              .Flags}}OPTIONS:
+	{{range .Flags}}{{.}}
+	{{end}}{{end}}
+`
--- rss/main.go
+++ rss/main.go
@@ -3,22 +3,85 @@ package main
 import (
 	"fmt"
 	"github.com/SlyMarbo/rss"
+	"github.com/codegangsta/cli"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("USAGE: %s <feed-url>\n", os.Args[0])
+	const episodesCommandName = "episodes"
+	cli.AppHelpTemplate = apphelptmpl
+	cli.CommandHelpTemplate = cmdhelptmpl
+	app := &cli.App{
+		Author: "Craig Weber",
+		Commands: []cli.Command{
+			cli.Command{
+				Name:        "episodes",
+				ShortName:   "ep",
+				Description: "Print episode details",
+				Action: func(c *cli.Context) {
+					if len(c.Args()) < 1 {
+						fmt.Println(c.Command.Usage)
+						os.Exit(1)
+					}
+
+					feed, err := rss.Fetch(c.Args()[0])
+					if err != nil {
+						fmt.Fprintln(os.Stderr, err)
+						os.Exit(1)
+					}
+
+					for i, episode := range feed.Items {
+						fmt.Println("Ref:       ", len(feed.Items)-i-1)
+						fmt.Println("Episode id:", episode.ID)
+						fmt.Println("Title:     ", episode.Title)
+						fmt.Println("Date:      ", episode.Date)
+						if episode.Summary != "" {
+							fmt.Println("Summary:   ", episode.Summary)
+						}
+						fmt.Println("Link:      ", episode.Link)
+						fmt.Println()
+					}
+				},
+				Usage: os.Args[0] + " " + episodesCommandName + " <feed-url>",
+				// BashComplete
+				// Before
+				// Flags
+				// SkipFlagParsing
+				// Subcommands
+			},
+		},
+		Email: "weberc2@gmail.com",
+		Name:  "rss - a simple command-line rss browser",
+		Action: func(c *cli.Context) {
+			if len(c.Args()) < 1 {
+				fmt.Println(c.App.Usage)
+				os.Exit(1)
+			}
+
+			feed, err := rss.Fetch(c.Args()[0])
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+
+			fmt.Println("Title:      ", feed.Title)
+			fmt.Println("NickName:   ", feed.Nickname)
+			fmt.Println("Link:       ", feed.Link)
+			fmt.Println("Episodes:   ", len(feed.Items))
+			fmt.Println("Description:", feed.Description)
+		},
+		Usage: "rss <feed-url>",
+		// BashComplete
+		// Before
+		// CommandNotFound
+		// Compiled
+		// EnableBashCompletion
+		// Flags
+		// Version
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
-	feed, err := rss.Fetch(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Title:      ", feed.Title)
-	fmt.Println("Episodes:   ", len(feed.Items))
-	fmt.Println("Link:       ", feed.Link)
-	fmt.Println("Description:", feed.Description)
 }
